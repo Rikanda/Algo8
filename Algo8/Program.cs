c5ef3cb11d9fc924f7bf71fefcd8a7d470eb6d13
Natalya <soltk7@yandex.ru>
2021-09-23 14:36:26 +0300

    Homework 8

--- Algo8/Program.cs
+++ Algo8/Program.cs
@@ -6,7 +6,7 @@ namespace Algo8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // здесь будет выполненное ДЗ
         }
     }
 }
